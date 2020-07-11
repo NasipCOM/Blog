0d7827da4446ebc5cb9916153f1988ea37b4cce5
NasipCOM <salmahunov@mail.ru>
2020-07-11 15:21:16 +0600

    Post from

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import View
+from .forms import PostForm     
 
 # Create your views here.
 def posts_list(request):
@@ -8,6 +12,29 @@ def posts_list(request):
     return render(request, 'blog/index.html', context={'posts':posts})
 
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post':post})
+# def post_detail(request, slug):
+#     return render(request, 'blog/post_detail.html', context={'post':post})
+
+class PostCreate(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_create_form.html', context={'form':form})
+    
+    
+    def post(self, request):
+        bound_form = PostForm(request.POST)
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        return render(request, 'blog/post_create_form.html', context={'form':bound_form})
+
+
+class PostDetail(View):
+    def get(self, request, slug):
+        post = get_object_or_404(Post, slug__iexact=slug)
+        return render(request, 'blog/post_detail.html', context={'post':post})
+
+
+# def post_create(self, request):
+#     form = PostForm()
+#     return render(request, 'blog/post_create_form.html', context={'form':form})
